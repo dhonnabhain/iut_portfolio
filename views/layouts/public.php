9c5ff153cf2bda0e4001edff527f72758a2f4974
Donovan <donovan.broquin@outlook.com>
2021-11-12 17:25:28 +0100

    init logged banner

--- views/layouts/public.php
+++ views/layouts/public.php
@@ -9,7 +9,13 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="relative">
+    <?php
+    if (isset($_SESSION['user'])) {
+        require __DIR__ . "/../partials/loggedBanner.php";
+    }
+    ?>
+
     <?php require(__DIR__ . "/../pages/$file") ?>
 </body>
 
---
+++ views/partials/loggedBanner.php
@@ -0,0 +1,6 @@
+<div class="sticky bg-gray-700 text-white top-0 z-50 w-full py-3 shadow-lg">
+    <div class="max-w-7xl mx-auto flex justify-between">
+        <p>Hello?</p>
+        <p>Hello?</p>
+    </div>
+</div>
