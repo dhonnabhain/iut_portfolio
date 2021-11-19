99ead1cb5647c924a194efc5194d3966994981ea
Donovan <donovan.broquin@outlook.com>
2021-11-19 15:59:44 +0100

    finish loggedBanner

--- views/partials/admin/header.php
+++ views/partials/admin/header.php
@@ -1,8 +1,14 @@
 <div class="flex justify-between bg-gray-800 px-6 py-3 shadow-lg">
-    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
+    <div class="flex">
+        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
+        <a href="/" class="flex self-center text-white text-sm hover:text-indigo-300 transition duration-150 ml-4">
+            <i class="fas fa-external-link-alt self-center mr-2"></i>
+            See site
+        </a>
+    </div>
 
     <div class="flex text-gray-50 space-x-3 hover:text-indigo-400 transition">
-        <a class="text-sm self-center" href="/logout">Déconnexion</a>
+        <a class="text-sm self-center" href="/logout">Logout</a>
         <i class="fas fa-sign-out-alt self-center"></i>
     </div>
 </div>
--- views/partials/loggedBanner.php
+++ views/partials/loggedBanner.php
@@ -1,6 +1,18 @@
 <div class="sticky bg-gray-700 text-white top-0 z-50 w-full py-3 shadow-lg">
-    <div class="max-w-7xl mx-auto flex justify-between">
-        <p>Hello?</p>
-        <p>Hello?</p>
+    <div class="max-w-7xl mx-auto flex justify-between text-sm">
+        <a href="/admin" class="flex hover:text-indigo-300 transition duration-150">
+            <i class="fas fa-external-link-alt self-center mr-2"></i>
+            Go back to admin
+        </a>
+
+
+        <div class="flex space-x-4">
+            <p>Logged as <?= $_SESSION['user']['login'] ?> (<?= $_SESSION['user']['email'] ?>)</p>
+            <div>|</div>
+            <a class="self-center hover:text-indigo-300 transition duration-150" href="/logout">
+                Logout
+                <i class="fas fa-sign-out-alt self-center ml-1"></i>
+            </a>
+        </div>
     </div>
 </div>
